d2082ecf38b8e3d49a60945aab508a2df1b5cc22
pavel148 <86954161+pavel148@users.noreply.github.com>
2022-10-09 03:50:22 +0300

    Add files via upload
    Не работает вывод сообщения.

---
+++ demo/src/main/java/io/proj3ct/demo/config/BotInitializer.java
@@ -0,0 +1,25 @@
+package io.proj3ct.demo.config;
+
+import io.proj3ct.demo.servise.TelegramBot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.*;
+@Component
+public class BotInitializer {
+    @Autowired
+    TelegramBot bot;
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException{
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(bot);
+        }
+        catch (TelegramApiException e){
+
+        }
+    }
+}
