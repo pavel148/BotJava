6fe1fda9a4641a0d72d7b7f58333941be0699b0c
pavel148 <86954161+pavel148@users.noreply.github.com>
2022-10-09 16:02:51 +0300

    Add files via upload
    исправлены ошибки. Добавлены логи. Дописан конструктор

--- demo/src/main/java/io/proj3ct/demo/servise/TelegramBot.java
+++ demo/src/main/java/io/proj3ct/demo/servise/TelegramBot.java
@@ -1,18 +1,40 @@
 package io.proj3ct.demo.servise;
 
 import io.proj3ct.demo.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import java.util.ArrayList;
+import java.util.List;
 
+@Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
+
     final BotConfig config;
 
-    public TelegramBot(BotConfig config){this.config=config;}
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+        List<BotCommand> listofCommands = new ArrayList<>();
+        listofCommands.add(new BotCommand("/start", "get a welcome message"));
+        listofCommands.add(new BotCommand("/mydata", "get your data stored"));
+        listofCommands.add(new BotCommand("/deletedata", "delete my data"));
+        listofCommands.add(new BotCommand("/help", "info how to use this bot"));
+        listofCommands.add(new BotCommand("/settings", "set your preferences"));
+        try {
+            this.execute(new SetMyCommands(listofCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error setting bot's command list: " + e.getMessage());
+        }
+    }
     @Override
     public String getBotUsername() {
         return config.getBotName();
@@ -39,7 +61,8 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
     private void  startCommandReceived(long chatId, String name ) {
-    String answer="Hi"+name+"nice to meet you!@";
+    String answer="Hi "+name+" nice to meet you!@";
+    log.info("Replaid to user "+name);
     sendMessage(chatId,answer);
     }
     private void sendMessage(long chatId, String textToSend){
@@ -50,7 +73,7 @@ public class TelegramBot extends TelegramLongPollingBot {
             execute(message);
         }
         catch (TelegramApiException e){
-                throw new RuntimeException(e);
+             log.error("Error occurred: "+e.getMessage());
         }
     }
 
